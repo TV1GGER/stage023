5612f9eda57f8ebee20b28bfaaa3c34332fd81da
Dzianis Perahudau <tvigger@yandex.ru>
2023-10-01 21:25:34 +0300

    feat: add get images from API

---
+++ image-galery/js/index.js
@@ -0,0 +1,55 @@
+let searchText = 'машина';
+const mainItem1 = document.querySelector('.main-item1');
+const mainItem2 = document.querySelector('.main-item2');
+const mainItem3 = document.querySelector('.main-item3');
+const mainItem4 = document.querySelector('.main-item4');
+const mainItem5 = document.querySelector('.main-item5');
+const mainItem6 = document.querySelector('.main-item6');
+const mainItem7 = document.querySelector('.main-item7');
+const mainItem8 = document.querySelector('.main-item8');
+const mainItem9 = document.querySelector('.main-item9');
+const mainItem10 = document.querySelector('.main-item10');
+const mainItem11 = document.querySelector('.main-item11');
+const mainItem12 = document.querySelector('.main-item12');
+const headerInputIn = document.querySelector('.header-input-in');
+const mainContainer = document.querySelector('.main-container');
+
+let imgArr = [mainItem1, mainItem2, mainItem3, mainItem4, mainItem5, mainItem6, mainItem7, mainItem8, mainItem9, mainItem10, mainItem11, mainItem12];
+
+let count =0;
+function getRandomNum(min, max) {
+    const minM = Math.ceil(min);
+    const maxM = Math.floor(max);
+    return Math.floor(Math.random() * (maxM - minM + 1)) + minM;
+    
+};
+
+
+getRandomNum(0, 25);
+
+let inputVal = '';
+headerInputIn.addEventListener('change', ()=>{
+    mainContainer.classList.add('main-container-active');
+    inputVal = headerInputIn.value;
+    setImg();
+});
+
+
+async function getLinkToImageFlickr(x) {
+    
+    const urlFlickr = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=9d30ebd21aae4bd52f018e7b4195d26b&tags=${inputVal}&extras=url_l&format=json&nojsoncallback=1`;
+const resFlickr = await fetch(urlFlickr);
+const dataFlickr = await resFlickr.json();
+let backgroundImage = ``;
+let imgFlr = document.createElement('img');
+let bodyFlickr = document.querySelector('body');
+imgFlr.src = dataFlickr.photos.photo[getRandomNum(1, 50)].url_l;
+x.style.backgroundImage = `url(${imgFlr.src})`;
+}
+
+function setImg() {
+    for(let key in imgArr) {
+        getLinkToImageFlickr(imgArr[key]);
+    }
+}
+
